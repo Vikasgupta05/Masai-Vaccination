34245cd80d5454cd9a224b4f23c649932213cac7
vikas <vikasnwrdr2001@gmail.com>
2022-10-13 14:10:27 +0530

    adding filter

--- src/component/dashboard.jsx
+++ src/component/dashboard.jsx
@@ -4,22 +4,57 @@ import { useEffect } from "react";
 
 export const Dashboard = () => {
 
-    var [count , setcount] = useState(0)
-    var data= JSON.parse(localStorage.getItem("data")) || [];
-
-    // useEffect(() => {
-    //     handeldelete()
-    // },[count])
+    var [vacData , setvacData] = useState([])
+    var [ data  , setdata ]=  useState( JSON.parse(localStorage.getItem("data")) || [])
+    var [status , setStatus] = useState(true)
 
     const handeldelete = (index) => {
-        data.splice(index,1)
-        localStorage.setItem("data" , JSON.stringify(data))
+        // data.splice(index,1)
+        // localStorage.setItem("data" , JSON.stringify(data))
         // setcount(count+1)
+    }
+
+
+    var vaccinedata = localStorage.setItem("vaccine", JSON.stringify(vacData)) || []
+
+    const Handelvaccine = (el) => {
+        vaccinedata.push(el);
+        localStorage.setItem("vaccine", JSON.stringify(vaccinedata))
+        alert("vaccination succesfully")
+    }
+
+    const Handelage = () => {
+
+    }
+
+
+    function handel_name(){
+        let All_data = data  
+      
+        if(status == true){
+            All_data.sort( function (a,b){
+              return Number(a.id) - Number(b.id);
+            })
+            setStatus(false)
+        }
+        else if(status == false){
+            All_data.sort( function (a,b){
+                return  Number(b.id)-Number(a.id);
+            })
+            setStatus(true)
+        }
+        setdata(All_data);
+      
+        console.log("asaa",All_data)
     }
 
     return(
         <div>
         <h1>DASHBOARD</h1>
+
+            <button
+                onClick={handel_name}
+            >sort by name</button>
 
             <div className="maindiv">
                 <table className="table">
@@ -46,9 +81,14 @@ export const Dashboard = () => {
                                         <td className="table">{el.priority}</td>
                                         <td className="table">{el.designation}</td>
                                         <td className="table">{el.vaccine}</td>
-                                        <td className="table"><button>Delete</button></td>
+                                        <td className="table"><button
+                                            //   onClick={handeldelete(index)}
+                                        >Delete</button></td>
                                         <td className="table"><button 
-                                                onClick={handeldelete(index)}
+                                                onClick={(() => {
+                                                    Handelvaccine(el)
+                                                })}
+
                                             >Vaccinate</button></td>
 
 
--- src/component/vaccinated.jsx
+++ src/component/vaccinated.jsx
@@ -1,9 +1,47 @@
 import React from "react";
 
 export const Vaccinated = () => {
+
+    var vaccine= JSON.parse(localStorage.getItem("vaccine")) || [];
+    
+
+
     return(
         <div>
             <h1>Vaccinated</h1>
+
+            <div className="maindiv">
+                <table className="table">
+                        <thead>
+                            <tr>
+                                <th className="table">id</th>
+                                <th  className="table">Name</th>
+                                <th  className="table">Age</th>
+                                <th  className="table">Designation</th>
+                                <th  className="table">Priority</th>
+                                <th  className="table">Vaccine</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                vaccine?.map((el , index ) => {
+                                return (
+                                    <tr key={index}>
+                                        <td className="table">{el.unique}</td>
+                                        <td className="table">{el.name}</td>
+                                        <td className="table">{el.age}</td>
+                                        <td className="table">{el.priority}</td>
+                                        <td className="table">{el.designation}</td>
+                                        <td className="table">{el.vaccine}</td>
+                                    </tr>
+                                )
+                                })
+                            }
+                        </tbody>
+                </table>
+            </div>
+
+
         </div>
     )
 }
