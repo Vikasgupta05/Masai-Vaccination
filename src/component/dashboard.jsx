b4c18659e905d74a71b6d4e75c8b23f97cd7794c
vikas <vikasnwrdr2001@gmail.com>
2022-10-13 13:15:08 +0530

    add delete funtionlity

--- src/component/dashboard.jsx
+++ src/component/dashboard.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 
 export const Dashboard = () => {
 
+    var [count , setcount] = useState(0)
     var data= JSON.parse(localStorage.getItem("data")) || [];
 
+    // useEffect(() => {
+    //     handeldelete()
+    // },[count])
+
+    const handeldelete = (index) => {
+        data.splice(index,1)
+        localStorage.setItem("data" , JSON.stringify(data))
+        // setcount(count+1)
+    }
 
     return(
         <div>
@@ -19,22 +31,27 @@ export const Dashboard = () => {
                                 <th  className="table">Designation</th>
                                 <th  className="table">Priority</th>
                                 <th  className="table">Vaccine</th>
-
-
-                                
+                                <th  className="table">Delete</th>
+                                <th  className="table">Vaccinate</th>
                             </tr>
                         </thead>
                         <tbody>
                             {
-                                data?.map((el , i ) => {
+                                data?.map((el , index ) => {
                                 return (
-                                    <tr key={i}>
+                                    <tr key={index}>
                                         <td className="table">{el.unique}</td>
                                         <td className="table">{el.name}</td>
                                         <td className="table">{el.age}</td>
                                         <td className="table">{el.priority}</td>
                                         <td className="table">{el.designation}</td>
                                         <td className="table">{el.vaccine}</td>
+                                        <td className="table"><button>Delete</button></td>
+                                        <td className="table"><button 
+                                                onClick={handeldelete(index)}
+                                            >Vaccinate</button></td>
+
+
                                     </tr>
                                 )
                                 })
